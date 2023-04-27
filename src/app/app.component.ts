0037d3b661d57b3dc7cb072f6326e49626d548e4
AnonimoMC <messina.michele@iisgalvanimi.edu.it>
2023-04-27 12:55:11 +0000

    Consegna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animale } from './models/animale.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01_Angular_empty';
+  title: string
+  animali: Animale[] = [
+    {
+      "nome": "cane",
+      "verso": "bau",
+      "zampe": 4,
+      "movimento": "cammina"
+    },
+    {
+      "nome": "rana",
+      "verso": "cra",
+      "zampe": 4,
+      "movimento": "salta"
+    },
+    {
+      "nome": "canarino",
+      "verso": "fiu",
+      "zampe": 2,
+      "movimento": "vola"
+    },
+    {
+      "nome": "serpente",
+      "verso": "fss",
+      "zampe": 0,
+      "movimento": "striscia"
+    }
+  ];
+
+  ordinaPerNome() {
+    this.animali.sort((a, b) => a.nome.localeCompare(b.nome));
+  }
+
+  ordinaPerZampe() {
+    this.animali.sort((a, b) => b.zampe - a.zampe);
+  }
+
+  coloreZampe(zampe: number): string {
+    if (zampe === 0) {
+      return 'green';
+    } else if (zampe === 2) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimaleComponent } from './animale/animale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimaleComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +18,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
